20990883685fc3cec123793cb3384fd08b86770f
FlorinPintilii <florin.pintilii@stud.acs.upb.ro>
2023-01-07 22:53:21 +0200

    costuri

--- Monstrii_si_Lei.cpp
+++ Monstrii_si_Lei.cpp
@@ -13,7 +13,7 @@ using namespace std;
 #define NF 132  //nr figuranti
 #define NC 735 //nr cast initial
 
-class Costuri
+class Preturi
 {
     protected:
         int c_vamp = 230;
@@ -24,14 +24,36 @@ class Costuri
         int c_orice = 40;
         int c_veg = 33;
         int c_flex = 46;
+        int c_autocar = 5680;
+        int c_2p = 350;
+        int c_3p = 420;
+        int c_castel = 10000;
 
+    public:
+        virtual void CSV() = 0;
+};
+
+class Costuri : public Preturi
+{
+    protected:
         int vamp, var, sir, clar, om;
         int orice, veg, flex;
+        int autocar, machiaj, mancare, cazare, lichide;
+        int c30, c45, c60, c100;
     
     public:
+        Costuri(){};
+        ~Costuri(){};
+
         void Apetit();
         void Machiaj();
-        void updateCSV(int rollnum, int index, string nou);
+        void Autocare();
+        void Cazare();
+        void Lichide();
+        void Castel();
+        void Dolari(float curs);
+        void CSV();
+        void CSV(int rollnum, int index, string nou);
 };
 
 void Costuri::Apetit()
@@ -47,17 +69,17 @@ void Costuri::Apetit()
         if(random == 0)
         {
             a++;
-            updateCSV(rollnum, 2, "Apt sa manance orice");
+            CSV(rollnum, 2, "Apt sa manance orice");
         }
         else if(random == 1)
         {
             b++;
-            updateCSV(rollnum, 2, "Vegetarian");
+            CSV(rollnum, 2, "Vegetarian");
         }
         else if(random == 2)
         {
             c++;
-            updateCSV(rollnum, 2, "Flexitarian");
+            CSV(rollnum, 2, "Flexitarian");
         }
         rollnum++;
     }
@@ -66,6 +88,7 @@ void Costuri::Apetit()
     this->veg = b;
     this->flex = c;
 
+    this->mancare = orice*c_orice + veg*c_veg + flex*c_flex;
 }
 
 void Costuri::Machiaj()
@@ -81,27 +104,27 @@ void Costuri::Machiaj()
         if(random == 0)
         {
             a++;
-            updateCSV(rollnum, 3, "Vampir");
+            CSV(rollnum, 3, "Vampir");
         }
         else if(random == 1)
         {
             b++;
-            updateCSV(rollnum, 3, "Varcolac");
+            CSV(rollnum, 3, "Varcolac");
         }
         else if(random == 2)
         {
             c++;
-            updateCSV(rollnum, 3, "Sirena");
+            CSV(rollnum, 3, "Sirena");
         }
         else if(random == 3)
         {
             d++;
-            updateCSV(rollnum, 3, "Clarvazator");
+            CSV(rollnum, 3, "Clarvazator");
         }
         else if(random == 4)
         {
             e++;
-            updateCSV(rollnum, 3, "Om");
+            CSV(rollnum, 3, "Om");
         }
         rollnum++;
     }
@@ -115,27 +138,27 @@ void Costuri::Machiaj()
         if(random == 0)
         {
             a++;
-            updateCSV(rollnum, 3, "Vampir");
+            CSV(rollnum, 3, "Vampir");
         }
         else if(random == 1)
         {
             b++;
-            updateCSV(rollnum, 3, "Varcolac");
+            CSV(rollnum, 3, "Varcolac");
         }
         else if(random == 2)
         {
             c++;
-            updateCSV(rollnum, 3, "Sirena");
+            CSV(rollnum, 3, "Sirena");
         }
         else if(random == 3)
         {
             d++;
-            updateCSV(rollnum, 3, "Clarvazator");
+            CSV(rollnum, 3, "Clarvazator");
         }
         else if(random == 4)
         {
             e++;
-            updateCSV(rollnum, 3, "Om");
+            CSV(rollnum, 3, "Om");
         }
         rollnum++;
     }
@@ -145,9 +168,100 @@ void Costuri::Machiaj()
     this->sir = c;
     this->clar = d;
     this->om = e;
+
+    this->machiaj = vamp*c_vamp + var*c_var + sir*c_sir + clar*c_clar + om*c_om;
 }
 
-void Costuri::updateCSV(int rollnum, int index, string nou)
+void Costuri::Autocare()
+{
+    int nr_autocare, autocare;
+
+    nr_autocare = N / 50;
+
+    try
+    {
+        if(nr_autocare < ((float)N/50.0))
+        {
+            throw nr_autocare;
+        }
+    }
+    catch(int nr_autocare)
+    {
+        autocare = nr_autocare + 1;
+    }
+
+    this->autocar = autocare * c_autocar * 2;
+}
+
+void Costuri::Cazare()
+{
+    int nr_camere2, camere2, nr_camere3;
+
+    nr_camere2 = NC / 2;
+    nr_camere3 = NF / 3;
+
+    try
+    {
+        if(nr_camere2 < ((float)N/2.0))
+        {
+            throw nr_camere2;
+        }
+    }
+    catch(int nr_camere2)
+    {
+        camere2 = nr_camere2 + 1;
+    }
+
+    this->cazare = camere2*c_2p + nr_camere3*c_3p;
+}
+
+void Costuri::Lichide()
+{
+    this->lichide = N * (3 + 0.5 * 30 + 0.8 * 4);
+}
+
+void Costuri::Castel()
+{
+    int c10;
+
+    c10 = (c_castel*10)*0.98;
+
+    this->c30 = c10 * 3;
+    this->c45 = c10 * 4 + c_castel * 5;
+    this->c60 = c10 * 6;
+    this->c100 = c10 * 10;
+}
+
+void Costuri::Dolari(float curs)
+{
+    this->autocar = this->autocar * curs;
+    this->cazare = this->cazare * curs;
+    this->machiaj = this->machiaj * curs;
+    this->mancare = this->mancare * curs;
+    this->lichide = this->lichide * curs;
+    this->c30 = this->c30 * curs;
+    this->c45 = this->c45 * curs;
+    this->c60 = this->c60 * curs;
+    this->c100 = this->c100 * curs;
+}
+
+void Costuri::CSV()
+{
+    fstream fout;
+
+    fout.open("Costuri.csv", ios::out | ios::app);
+
+    fout << "Perioada" << "," << "30 Zile" << "," << "45 Zile" << "," << "60 Zile" << "," << "100 Zile" << "\n";
+    fout << "Transport" << "," << autocar << "," << autocar << "," << autocar << "," << autocar << "\n";
+    fout << "Cazare" << "," << cazare*30 << "," << cazare*45 << "," << cazare*60 << "," << cazare*100 << "\n";
+    fout << "Machiaj" << "," << machiaj*30 << "," << machiaj*45 << "," << machiaj*60 << "," << machiaj*100 << "\n";
+    fout << "Mancare si apa" << "," << (mancare+lichide)*30 << "," << (mancare+lichide)*45 << "," << (mancare+lichide)*60 << "," << (mancare+lichide)*100 << "\n";
+    fout << "Inchiriere spatiu" << "," << c30 << "," << c45 << "," << c60 << "," << c100 << "\n";
+
+    fout.close();
+}
+
+void Costuri::CSV(int rollnum, int index, string nou)
 {
 	fstream fin, fout;
 
@@ -273,26 +387,7 @@ void addExtra()
     }
 }
 
-int Autocare()
-{
-    int nr_autocare, autocare;
 
-    nr_autocare = N / 50;
-
-    try
-    {
-        if(nr_autocare < ((float)N/50.0))
-        {
-            throw nr_autocare;
-        }
-    }
-    catch(int nr_autocare)
-    {
-        autocare = nr_autocare + 1;
-    }
-
-    return autocare;
-}
 
 int main()
 {
@@ -301,18 +396,14 @@ int main()
     addExtra();
 
     Costuri pret;
-    pret.Apetit();
+    pret.Autocare();
+    pret.Cazare();
     pret.Machiaj();
-
-    /*int nr_autocare, cost_autocare;
-    int cost_machiaj;
-
-    nr_autocare = Autocare();
-    cost_autocare = nr_autocare * 5680;
-    cout << cost_autocare << endl;
-
-    cost_machiaj = Monstri();
-    cout << cost_machiaj << endl;*/
+    pret.Apetit();
+    pret.Lichide();
+    pret.Castel();
+    pret.Dolari(0.216);
+    pret.CSV();
 
     return 0;
 }
